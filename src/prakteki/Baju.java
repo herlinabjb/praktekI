d65d3157b9ac3c4b88805d77951397959b297c79
Herlina <https://github.com/herlinabjb>
2018-03-27 00:50:11 -0700

    membuat atribut dan behavior

--- src/prakteki/Baju.java
+++ src/prakteki/Baju.java
@@ -10,5 +10,19 @@ package prakteki;
  * @author Asus A456UR
  */
 public class Baju {
+    String Merk;
+    String Ukuran;
+    String Warna;
+    String Model;
+    String Jenis_kain;
     
+    void cetakInfo(){
+        System.out.println("Merk :"+Merk);
+        System.out.println("Ukuran :"+Ukuran);
+        System.out.println("Warna :"+Warna);
+        System.out.println("Model :"+Model);
+        System.out.println("Jenis_kain :"+Jenis_kain);
+        
+        
+    }
 }
